9d94a13f73843df1582d4e953c295236d35e2253
demyslima <demys.lima@contasimples.com>
2022-02-14 13:51:57 -0300

    chore: including storybook jest testing-in-library

---
+++ src/modules/About/About.spec.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { act } from 'react-dom/test-utils'
+import About from './index'
+
+describe('About', function () {
+  it('should display about with just jest', function () {
+    let container = document.createElement('div')
+    document.body.appendChild(container)
+    act(() => {
+      ReactDOM.render(<About />, container)
+    })
+    const header = container.querySelector('h1')
+    expect(header?.textContent).toBe('About')
+  })
+
+  it('should display about with testing in library', function () {
+    const { container } = render(<About />)
+    const header = container.querySelector('h1')
+    expect(header).toHaveTextContent('About')
+  })
+})
